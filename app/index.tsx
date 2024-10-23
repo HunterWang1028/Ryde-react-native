15082b98b1a542a7559c981179900dc97d1f874f
Hunter Wang <s092284s@gmail.com>
2024-10-23 23:44:02 +0800

    onboarding screens

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { Redirect } from "expo-router";
 
 const Home = () => {
-  return <Redirect href="/(auth)welcome" />;
+  return <Redirect href={"/(auth)/welcome"} />;
 };
 export default Home;
